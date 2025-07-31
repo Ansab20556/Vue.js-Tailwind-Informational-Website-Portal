3c4337fc640abddac21934da72dc5693a4d57930
Ansab Naif <ansab20556@gmail.com>
2025-07-31 22:12:31 +0300

    add a new section under the hero section in home page

---
+++ src/components/ArticleCard.vue
@@ -0,0 +1,25 @@
+<!-- components/ArticleCard.vue -->
+<template>
+    <a :href="link"
+        class="bg-white rounded shadow hover:-translate-y-1 duration-300 dark:bg-gray-800">
+
+        <img :src="image" :alt="alt" class="rounded-t w-full" />
+        <div class="p-4">
+        <h3 class="font-bold text-lg mb-1">{{ title }}</h3>
+        <p v-if="category" class="text-sm text-gray-600 dark:text-gray-300">{{ category }}</p>
+        </div>
+    </a>
+</template>
+
+<script setup>
+defineProps({
+    image: String,
+    alt: String,
+    title: String,
+    category: String,
+    link: {
+        type: String,
+        default: "#"
+    }
+})
+</script>
---
+++ src/components/ArticlesSection.vue
@@ -0,0 +1,44 @@
+<!-- ArticlesSection.vue -->
+<template>
+  <section class="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-6 p-6">
+        <ArticleCard
+            v-for="(article, index) in articles"
+            :key="index"
+            :image="article.image"
+            :alt="article.alt"
+            :title="article.title"
+            :category="article.category"
+            :link="article.link"/>
+  </section>
+</template>
+
+<script setup>
+import ArticleCard from './ArticleCard.vue'
+import Image1 from '../assets/images/image1.png'
+import Image2 from '../assets/images/image2.png'
+import Image3 from '../assets/images/image3.png'
+
+const articles = [
+  {
+    image: Image1,
+    alt: 'صورة 1',
+    title: 'بيان بشأن حادث قافلة شمال غزة',
+    category: 'بيانات صحفية',
+    link: '#'
+  },
+  {
+    image:Image2 ,
+    alt: 'صورة 2',
+    title: 'الحرب في أوكرانيا: كيف يساعد البرنامج صغار المزارعين',
+    category: 'قصة',
+    link: '#'
+  },
+  {
+    image: Image3,
+    alt: 'صورة 3',
+    title: 'اللاجئون الفارون من السودان يواجهون تفاقم الجوع',
+    category: 'بيانات صحفية',
+    link: '#'
+  }
+]
+</script>
