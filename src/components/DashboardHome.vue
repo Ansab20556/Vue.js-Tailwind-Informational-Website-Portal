66c81730ec12a905205efb1229c0f2d78f7dc508
Ansab Naif <ansab20556@gmail.com>
2025-08-02 14:06:03 +0300

    add dashboard home

--- src/components/DashboardHome.vue
+++ src/components/DashboardHome.vue
@@ -1,31 +1,47 @@
 <!-- src/components/DashboardHome.vue -->
 <template>
-  <div class="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 gap-6">
-    <div class=" bg-white dark:bg-gray-800 p-6 rounded shadow text-center hover:-translate-y-1 duration-300">
-      <div class="text-3xl font-bold text-blue-600">{{ messageCount }}</div>
-      <div class="text-gray-600 dark:text-gray-300 mt-2">الرسائل</div>
+    <div class="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 gap-6">
+        <div class=" bg-white dark:bg-gray-800 p-6 rounded shadow text-center hover:-translate-y-1 duration-300">
+            <div class="text-3xl font-bold text-blue-600">{{ messageCount }}</div>
+            <div class="text-gray-600 dark:text-gray-300 mt-2">الرسائل</div>
+        </div>
+        <div class=" bg-white dark:bg-gray-800 p-6 rounded shadow text-center hover:-translate-y-1 duration-300">
+            <div class="text-3xl font-bold text-green-600">{{ projectsCount }}</div>
+            <div class="text-gray-600 dark:text-gray-300 mt-2">عدد المشاريع</div>
+        </div>
+        <div class=" bg-white dark:bg-gray-800 p-6 rounded shadow text-center hover:-translate-y-1 duration-300">
+            <div class="text-3xl font-bold text-pink-500"> ${{ totalAmount }}</div>
+            <div class="text-gray-600 dark:text-gray-300 mt-2">اجمالي التبرعات</div>
+        </div>
+        <div class=" bg-white dark:bg-gray-800 p-6 rounded shadow text-center hover:-translate-y-1 duration-300">
+            <div class="text-3xl font-bold text-yellow-500">12</div>
+            <div class="text-gray-600 dark:text-gray-300 mt-2">المستخدمون</div>
+        </div>
+        
+
     </div>
-    <div class=" bg-white dark:bg-gray-800 p-6 rounded shadow text-center hover:-translate-y-1 duration-300">
-      <div class="text-3xl font-bold text-green-600">{{ visitorCount }}</div>
-      <div class="text-gray-600 dark:text-gray-300 mt-2">الزوار</div>
+    <div class="grid grid-cols-1 sm:grid-cols-1 lg:grid-cols-2 gap-6 py-12">
+        <RecentDonors></RecentDonors>
+        <DonationsTypeChart></DonationsTypeChart>
+        
     </div>
-    <div class=" bg-white dark:bg-gray-800 p-6 rounded shadow text-center hover:-translate-y-1 duration-300">
-      <div class="text-3xl font-bold text-red-600">{{ donationCount }}</div>
-      <div class="text-gray-600 dark:text-gray-300 mt-2">التبرعات</div>
-    </div>
-    <div class=" bg-white dark:bg-gray-800 p-6 rounded shadow text-center hover:-translate-y-1 duration-300">
-      <div class="text-3xl font-bold text-yellow-500">12</div>
-      <div class="text-gray-600 dark:text-gray-300 mt-2">المستخدمون</div>
-    </div>
-  </div>
+    <DonationsChart></DonationsChart>
+    
 </template>
 
 <script setup>
 import { ref, onMounted } from 'vue'
+import RecentDonors from './RecentDonors.vue'
+import DonationsChart from './DonationsChart.vue'
+import DonationsTypeChart from './DonationsTypeChart.vue'
+
 
 const messageCount = ref(0)
 const visitorCount = ref(0)
 const donationCount = ref(0)
+const projectsCount = ref(0)
+const donations = ref([])
+const totalAmount = ref(0)
 
 onMounted(() => {
   // عدد الرسائل
@@ -39,5 +55,13 @@ onMounted(() => {
   // عدد التبرعات (نفرض أنك تحفظ كل تبرع بنفس الطريقة)
   const donations = JSON.parse(localStorage.getItem("donations") || "[]")
   donationCount.value = donations.length
+
+const projects = JSON.parse(localStorage.getItem("workFields") || "[]")
+  projectsCount.value = projects.length
+
+donations.value = JSON.parse(localStorage.getItem('donations') || '[]')
+
+totalAmount.value = donations.value.reduce((sum, d) => sum + Number(d.amount), 0)
 })
+
 </script>
